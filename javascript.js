edafc23c5c82b9a7ba4f82b522f6127eda3c2616
Carmen Collins <hellocarmencollins@gmail.com>
2018-08-06 12:54:48 -0500

    adding functionality so when user clicks a button, gifs and their ratings are displayed

--- javascript.js
+++ javascript.js
@@ -1,17 +1,41 @@
-var disneyMovies = [];
+var disneyMovies = ["Lion King", "The Little Mermaid", "Finding Nemo"];
 
 // Function for displaying movie buttons
 function renderButtons() {
     $("#buttons-div").empty();
 
     for (var i = 0; i < disneyMovies.length; i++) {
-        var newBtn = $("<button>");
-        newBtn.addClass("movie");
+        var newBtn = $("<button class='movie'>");
         newBtn.attr("movie-name", disneyMovies[i]);
         newBtn.text(disneyMovies[i]);
         $("#buttons-div").append(newBtn);
     };
 };
+
+// Function for displaying GIFs
+$(document.body).on("click", ".movie", function() {
+    var movieName = $(this).attr("movie-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    movieName + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }). then(function(response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var gif = $("<img>");
+            gif.attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(p);
+            gifDiv.append(gif);
+            $("#gifs-div").prepend(gifDiv);
+        }
+    })
+
+});
 
 // Function for adding movies to the array
 $("#add-movie").on("click", function(event) {
@@ -19,4 +43,8 @@ $("#add-movie").on("click", function(event) {
     var movie = $("#user-input").val().trim();
     disneyMovies.push(movie);
     renderButtons();
+    $("#user-input").val("");
 });
+
+// Function for displaying the buttons that are already on the page
+renderButtons();
