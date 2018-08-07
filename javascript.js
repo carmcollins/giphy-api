4cea44ef181e2c7eef3cbe0d3405cebb36852e65
Carmen Collins <hellocarmencollins@gmail.com>
2018-08-06 23:44:47 -0500

    styling the webpage

--- javascript.js
+++ javascript.js
@@ -22,19 +22,38 @@ $(document.body).on("click", ".movie", function() {
         url: queryURL,
         method: "GET"
     }). then(function(response) {
+        console.log(response);
         var results = response.data;
         for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");
+            gifDiv.addClass("gif-display");
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
+            p.addClass("rating");
             var gif = $("<img>");
-            gif.attr("src", results[i].images.fixed_height.url);
+            gif.attr("src", results[i].images.fixed_height_still.url);
+            gif.attr("state", "still");
+            gif.addClass("gif");
             gifDiv.append(p);
             gifDiv.append(gif);
             $("#gifs-div").prepend(gifDiv);
         }
     })
+});
 
+// Function for unpausing and pausing GIFs -- this isn't working???
+$(".gif").on("click", function() {
+    var state = $(this).attr("state");
+
+    if (state === "still") {
+        var animateURL = results[i].images.fixed_height.url
+        $(this).attr("src", animateURL);
+        $(this).attr("state", "animate");
+    } else if (state === "animate") {
+        var stillURL = results[i].images.fixed_height_still.url
+        $(this).attr("src", stillURL);
+        $(this).attr("state", "still");
+    }
 });
 
 // Function for adding movies to the array
